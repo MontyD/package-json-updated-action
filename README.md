81081126e63386aa48ff5780eb6d5458a6591da1
montyD <hello@montydawson.co.uk>
2019-12-29 14:09:54 +0000

    Add basic action for testing

---
+++ README.md
@@ -0,0 +1,9 @@
+# GitHub Action to detect changes in package.json version
+
+This action outputs a variable 'has-updated' indicating that the package.json was updated in the most recent commit
+
+## Outputs
+
+### `has-updated`
+
+Whether the package.json version has updated.
---
+++ index.js
@@ -0,0 +1,42 @@
+const { setOutput, getInput, setFailed } = require('@actions/core');
+const { GitHub, context } = require('@actions/github');
+
+const getPackageJson = async (ref, octokit) => {
+    const packageJSONData = (await octokit.repos.getContents({
+        ...context.repo,
+        path: 'package.json',
+        ref,
+    })).data.content;
+    if (!packageJSONData) {
+        throw new Error(`Could not find package.json for commit ${ref}`);
+    }
+    return JSON.parse(Buffer.from(packageJSONData, 'base64').toString());
+};
+
+const run = async () => {
+    const token = getInput('GITHUB_TOKEN');
+    if (!token) {
+        throw new Error('GITHUB_TOKEN not provided');
+    }
+
+    const octokit = new GitHub(token);
+    const currentRef = context.sha;
+    const previousRef = ((await octokit.repos.getCommit({
+        ...context.repo,
+        ref: currentRef,
+    })).data.parents[0] || {}).sha;
+
+    if (!previousRef) {
+        setOutput('has-updated', true);
+        return;
+    }
+
+    const currentPackageJSON = await getPackageJson(currentRef, octokit);
+    const previousPackageJSON = await getPackageJson(previousRef, octokit);
+
+    setOutput('has-updated', currentPackageJSON.version !== previousPackageJSON.version);
+}
+
+run().catch(error => {
+    setFailed(error.message);
+});
